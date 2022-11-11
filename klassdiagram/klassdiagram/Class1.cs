890d7a907403b8c130b26231ec0c935d17756bd9
Pablo <95082994+B14swe@users.noreply.github.com>
2022-11-11 11:16:12 +0100

    Add files via upload

---
+++ klassdiagram/klassdiagram/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klassdiagram
+{
+    public class Djur
+    {
+    
+        private int legs;
+        private int weight;
+
+
+        public int Getlegs() { return this.legs; }
+        public int GetWeight() { return this.weight; }
+
+        public void Setlegs(int legs) { this.legs = legs; }
+        public void SetWeight(int weight) { this.weight = weight; }
+        public class Hund : Djur
+        {
+            private string name;
+
+            public void SetName(string name) { this.name = name; }
+
+            public string GetName() { return this.name; }
+            public void woof()
+            {
+                Console.WriteLine(name + " " + "woofed");
+            }
+
+            public void walk()
+            {
+                Console.WriteLine(name + " " + "walked with his " + legs + " legs");
+            }
+        }
+
+    }
+}
---
+++ klassdiagram/klassdiagram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using klassdiagram;
+Djur.Hund harry = new Djur.Hund();
+
+harry.SetName("harry");
+harry.Setlegs(4);
+harry.woof();
+harry.walk();
+harry.SetWeight(30);
+Console.WriteLine((harry.GetWeight()));
+Console.WriteLine(harry.Getlegs());
